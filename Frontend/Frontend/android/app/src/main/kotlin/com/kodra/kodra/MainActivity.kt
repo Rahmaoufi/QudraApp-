3781b736f3375d57b5840764a9eed956e5eeb901
Rahmaoufi <125396750+Rahmaoufi@users.noreply.github.com>
2023-03-08 06:01:22 +0300

    Delete Frontend/Frontend directory

--- Frontend/Frontend/android/app/src/main/kotlin/com/kodra/kodra/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.kodra.kodra
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- Frontend/Frontend/lib/app/modules/home/controller/login_controller.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get/get.dart';
-
-class HomeController extends GetxController{
-bool isLogin=true;
-  setIsLogin(bool newVal){
-    isLogin=newVal;
-    update();
-  }
-}
